e30cd44d59674d0a4e7a1cd8a565af1c0d2836de
Lin Gao <aoingl@gmail.com>
2021-06-01 16:47:14 +0800

    prepare slides on vert.x talk next week

--- vertx-intro/server.js
+++ vertx-intro/server.js
@@ -2,6 +2,6 @@ vertx.createHttpServer()
   .requestHandler(function (req) {
     req.response()
       .putHeader("content-type", "text/plain")
-      .end("Hello from Vert.x!");
+      .end("Hello from Vert.x JS!");
   }).listen(8080);
   
